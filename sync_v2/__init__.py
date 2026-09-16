d62ce3238970cee5e4833a4711737be48be34185
zeithampa-oss <zeitham.pavel@gmail.com>
2026-09-16 17:42:27 +0200

    Add SYNC 2.0 core package

---
+++ sync_v2/__init__.py
@@ -0,0 +1,5 @@
+"""PZ-REVIZE SYNC 2.0 - offline-first multi-device synchronization core."""
+
+from .core import Change, ChangeKind, SyncEngine, SyncResult
+
+__all__ = ["Change", "ChangeKind", "SyncEngine", "SyncResult"]
